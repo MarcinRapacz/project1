4a9133b55069d7daf00d627b510342bfb8df8aef
unknown <marcin_04@onet.eu>
2019-08-27 15:20:37 +0200

    Facts fixed

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import {
   FaRegCheckSquare,
   FaBriefcase,
@@ -19,6 +19,44 @@ const Facts = () => {
     second: { color: "primary", text: "facts" },
   }
 
+  const [counters, setCounters] = useState([
+    {
+      number: 218,
+      icon: <FaRegCheckSquare />,
+      desc: "Completed projects",
+      active: true,
+    },
+    {
+      number: 360,
+      icon: <FaRegClock />,
+      desc: "hours of work",
+      desc2: "/month",
+      active: true,
+    },
+    {
+      number: 135,
+      icon: <FaTelegramPlane />,
+      desc: "Solved tickets",
+      active: true,
+    },
+    {
+      number: 174,
+      icon: <FaRegThumbsUp />,
+      desc: "Satisfied clients",
+      active: true,
+    },
+  ])
+
+  const unactiveSensor = unactiveCounter => {
+    if (unactiveCounter.active) {
+      unactiveCounter.active = false
+      const updatedArr = counters.map(counter =>
+        counter.desc === unactiveCounter.desc ? unactiveCounter : counter
+      )
+      setCounters(updatedArr)
+    }
+  }
+
   return (
     <section className="facts container" id="facts">
       <Decorator>
@@ -30,76 +68,32 @@ const Facts = () => {
         mollitia.
       </p>
       <div className="facts-container">
-        <div className="counter">
-          <div className="counter-header">
-            <span className="counter-square"></span>
-            <h1 className="counter-number">
-              <VisibilitySensor>
-                {({ isVisible }) => <CountUp end={isVisible ? 218 : 0} />}
-              </VisibilitySensor>
-            </h1>
-            <span className="counter-square"></span>
+        {counters.map(conter => (
+          <div className="counter" key={conter.desc}>
+            <div className="counter-header">
+              <span className="counter-square"></span>
+              <h1 className="counter-number">
+                <VisibilitySensor active={conter.active}>
+                  {({ isVisible }) => {
+                    isVisible && unactiveSensor(conter)
+
+                    return <CountUp end={isVisible ? conter.number : 0} />
+                  }}
+                </VisibilitySensor>
+              </h1>
+              <span className="counter-square"></span>
+            </div>
+            <div className="counter-icon">
+              <span className="counter-line"></span>
+              {conter.icon}
+              <span className="counter-line"></span>
+            </div>
+            <p className="counter-desc">
+              {conter.desc}
+              <small>{conter.desc2}</small>
+            </p>
           </div>
-          <div className="counter-icon">
-            <span className="counter-line"></span>
-            <FaRegCheckSquare />
-            <span className="counter-line"></span>
-          </div>
-          <p className="counter-desc">Completed projects</p>
-        </div>
-        <div className="counter">
-          <div className="counter-header">
-            <span className="counter-square counter-square--primary"></span>
-            <h1 className="counter-number">
-              <VisibilitySensor>
-                {({ isVisible }) => <CountUp end={isVisible ? 360 : 0} />}
-              </VisibilitySensor>
-            </h1>
-            <span className="counter-square counter-square--primary"></span>
-          </div>
-          <div className="counter-icon">
-            <span className="counter-line"></span>
-            <FaRegClock />
-            <span className="counter-line"></span>
-          </div>
-          <p className="counter-desc">
-            hours of work/<small>month</small>
-          </p>
-        </div>
-        <div className="counter">
-          <div className="counter-header">
-            <span className="counter-square"></span>
-            <h1 className="counter-number">
-              <VisibilitySensor>
-                {({ isVisible }) => <CountUp end={isVisible ? 135 : 0} />}
-              </VisibilitySensor>
-            </h1>
-            <span className="counter-square"></span>
-          </div>
-          <div className="counter-icon">
-            <span className="counter-line"></span>
-            <FaTelegramPlane />
-            <span className="counter-line"></span>
-          </div>
-          <p className="counter-desc">Solved tickets</p>
-        </div>
-        <div className="counter">
-          <div className="counter-header">
-            <span className="counter-square"></span>
-            <h1 className="counter-number">
-              <VisibilitySensor>
-                {({ isVisible }) => <CountUp end={isVisible ? 174 : 0} />}
-              </VisibilitySensor>
-            </h1>
-            <span className="counter-square"></span>
-          </div>
-          <div className="counter-icon">
-            <span className="counter-line"></span>
-            <FaRegThumbsUp />
-            <span className="counter-line"></span>
-          </div>
-          <p className="counter-desc">Satisfied clients</p>
-        </div>
+        ))}
       </div>
     </section>
   )
